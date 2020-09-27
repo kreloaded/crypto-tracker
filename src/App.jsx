ba52742a4919ef4c80acd8084b6caaba6d61c006
kreloaded <kiran@ost.com>
2020-09-27 19:51:41 +0530

    added css to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import CoinSummary from './pages/CoinSummary';
+import Header from './components/Header';
+import CoinDetail from './pages/CoinDetail';
+import './App.css'
 
 const App = () => {
     return (
         <div>
-            App
+            <BrowserRouter>
+            <Header />
+                <Route exact path="/" component={ CoinSummary }></Route>
+            </BrowserRouter>
         </div>
     );
 };
